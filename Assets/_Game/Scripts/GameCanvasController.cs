6425d989c85d03933cbdd38fa217d990e1603604
dzozrawr <jovan_96@live.ru>
2022-11-04 16:02:14 +0100

    added inceremntal money earning effect with sfx

--- Assets/_Game/Scripts/GameCanvasController.cs
+++ Assets/_Game/Scripts/GameCanvasController.cs
@@ -5,19 +5,60 @@ using TMPro;
 
 public class GameCanvasController : MonoBehaviour
 {
-    public TMP_Text coinAmountTxt=null;
+    public TMP_Text coinAmountTxt = null;
 
-    private GameController gameController=null;
+    public AnimationCurve moneyEarnEase;
+
+    private GameController gameController = null;
+    private Coroutine addMoneyInSequenceCoroutine = null;
     // Start is called before the first frame update
     void Start()
     {
-        gameController=GameController.Instance;
+        gameController = GameController.Instance;
 
-        gameController.CoinAmountChanged+=OnCoinAmountChanged;
+        gameController.CoinAmountChanged += OnMoneyAmountChanged;
     }
 
 
-    public void OnCoinAmountChanged(){
-        coinAmountTxt.text= GameController.CoinAmount+"";            
+    public void OnMoneyAmountChanged()
+    {
+        // coinAmountTxt.text= GameController.CoinAmount+"";
+        if(addMoneyInSequenceCoroutine==null)            
+        addMoneyInSequenceCoroutine = StartCoroutine(AddMoneyInSequence(int.Parse(coinAmountTxt.text), GameController.CoinAmount, 1f));
+        else{
+            StopCoroutine(addMoneyInSequenceCoroutine);
+            addMoneyInSequenceCoroutine=null;
+            addMoneyInSequenceCoroutine = StartCoroutine(AddMoneyInSequence(int.Parse(coinAmountTxt.text), GameController.CoinAmount, 1f));
+        }
+    }
+
+    IEnumerator AddMoneyInSequence(float curCoinAmount, float targetCoinAmount, float duration)
+    {
+        float startCoinAmount = curCoinAmount;
+
+        int prevCoinAmountInt = (int)curCoinAmount;
+        int curCoinAmountInt = (int)curCoinAmount;
+        for (float t = 0f, counter = 0f; t < 1f; counter += Time.deltaTime, t = counter / duration)
+        {
+            prevCoinAmountInt = (int)curCoinAmount;
+            curCoinAmount = Mathf.Lerp(startCoinAmount, targetCoinAmount, moneyEarnEase.Evaluate(t));
+            curCoinAmountInt = (int)curCoinAmount;
+
+            if (curCoinAmountInt != prevCoinAmountInt)
+            {
+                coinAmountTxt.text = (int)curCoinAmount + "";
+                SoundManager.Instance.PlaySound("coinClaim");
+            }
+
+            yield return null;
+        }
+
+        if (curCoinAmount != targetCoinAmount)
+        {
+            coinAmountTxt.text = (int)targetCoinAmount + "";
+            SoundManager.Instance.PlaySound("coinClaim");
+        }
+
+        addMoneyInSequenceCoroutine=null;
     }
 }
