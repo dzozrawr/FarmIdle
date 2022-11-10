a2378f872b1e58873b3e0397aa369c54867db129
xsshaddx <ensarsacirovic55@gmail.com>
2022-11-10 20:40:53 +0100

    Added all models, textures and prefabs for plants, fruits, vegetables and soil (all in their respective same named folders), made player smaller in FirstScene and moved camera closer, added new plant types to PlantInfo.cs script

--- Assets/_Game/Scripts/New Folder/PlantInfo.cs
+++ Assets/_Game/Scripts/New Folder/PlantInfo.cs
@@ -1,7 +1,7 @@
 public class PlantInfo 
 {
     public enum PlantType{
-        Sunflower, Tulip, Daffodil
+        Sunflower, Tulip, Daffodil, Pepper, Tomato, Pumpkin, Strawberry, Watermelon
     }
 
     private PlantType type;
