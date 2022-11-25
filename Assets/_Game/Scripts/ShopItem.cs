0583ce0de211244ec42b866e1792c94c0231e49a
dzozrawr <jovan_96@live.ru>
2022-11-25 16:13:55 +0100

    finished shop progress transfer to other levels

--- Assets/_Game/Scripts/ShopItem.cs
+++ Assets/_Game/Scripts/ShopItem.cs
@@ -25,7 +25,11 @@ public class ShopItem : MonoBehaviour
         if(priceInt<=GameController.CoinAmount){
             GameController.Instance.AddMoneyIncrementally(-priceInt);   //reduce the money in game controller
             shopController.BuyShopItem(type);//do the buying logic in shop controller
-            priceText.transform.parent.gameObject.SetActive(false); //apply the bought visual
+            SetBoughtVisual();
         }
+    }
+
+    public void SetBoughtVisual(){
+        priceText.transform.parent.gameObject.SetActive(false); //apply the bought visual
     }
 }
